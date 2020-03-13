8107752fc14480cfc7ef9f39bbedfb7ec2f702c8
Marius Schütte <marius.schuette@gmail.com>
2020-03-14 00:53:26 +0100

    Added FormBuilder with fluent interface

--- vicephp/Virtue-Forms/tests/Forms/FormBuilder/FormBuilderTest.php
+++ vicephp/Virtue-Forms/tests/Forms/FormBuilder/FormBuilderTest.php
@@ -32,5 +32,50 @@ class FormBuilderTest extends TestCase
 }
 JSON;
         $this->assertEquals($expected, json_encode($buildForm(), JSON_PRETTY_PRINT));
+
+        $buildForm->select(['name' => 'aSelectField'])
+            ->option(['label' => 'aLabel', 'value' => 'aValue'])
+            ->option(['label' => 'bLabel', 'value' => 'bValue']);
+
+        $expected = <<<JSON
+{
+    "element": "form",
+    "attributes": {
+        "name": "aForm"
+    },
+    "children": [
+        {
+            "element": "input",
+            "attributes": {
+                "type": "text",
+                "name": "aTextInput"
+            }
+        },
+        {
+            "element": "select",
+            "attributes": {
+                "name": "aSelectField"
+            },
+            "children": [
+                {
+                    "element": "option",
+                    "attributes": {
+                        "label": "aLabel",
+                        "value": "aValue"
+                    }
+                },
+                {
+                    "element": "option",
+                    "attributes": {
+                        "label": "bLabel",
+                        "value": "bValue"
+                    }
+                }
+            ]
+        }
+    ]
+}
+JSON;
+        $this->assertEquals($expected, json_encode($buildForm(), JSON_PRETTY_PRINT));
     }
 }
