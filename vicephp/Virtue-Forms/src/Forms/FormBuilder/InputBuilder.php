a162120186866674abcbb653c196ddce1996954f
Marius Schütte <marius.schuette@gmail.com>
2020-03-14 14:02:09 +0100

    Added FormBuilder with fluent interface

--- vicephp/Virtue-Forms/src/Forms/FormBuilder/InputBuilder.php
+++ vicephp/Virtue-Forms/src/Forms/FormBuilder/InputBuilder.php
@@ -12,7 +12,7 @@ class InputBuilder
      * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/button
      * @param array $attributes
      */
-    public function typeButton(array $attributes)
+    public function typeButton(array $attributes): void
     {
         $this->attributes = ['type' => 'button'] + $attributes;
     }
@@ -21,9 +21,99 @@ class InputBuilder
      * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox
      * @param array $attributes
      */
-    public function typeCheckbox(array $attributes)
+    public function typeCheckbox(array $attributes): void
     {
         $this->attributes = ['type' => 'checkbox'] + $attributes;
+    }
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/color
+     * @param array $attributes
+     */
+    public function typeColor(array $attributes): void
+    {
+        $this->attributes = ['type' => 'color'] + $attributes;
+    }
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/date
+     * @param array $attributes
+     */
+    public function typeDate(array $attributes): void
+    {
+        $this->attributes = ['type' => 'date'] + $attributes;
+    }
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/datetime-local
+     * @param array $attributes
+     */
+    public function typeDatetimeLocal(array $attributes): void
+    {
+        $this->attributes = ['type' => 'datetime-local'] + $attributes;
+    }
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email
+     * @param array $attributes
+     */
+    public function typeEmail(array $attributes): void
+    {
+        $this->attributes = ['type' => 'email'] + $attributes;
+    }
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file
+     * @param array $attributes
+     */
+    public function typeFile(array $attributes): void
+    {
+        $this->attributes = ['type' => 'file'] + $attributes;
+    }
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/hidden
+     * @param array $attributes
+     */
+    public function typeHidden(array $attributes): void
+    {
+        $this->attributes = ['type' => 'hidden'] + $attributes;
+    }
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/image
+     * @param array $attributes
+     */
+    public function typeImage(array $attributes): void
+    {
+        $this->attributes = ['type' => 'image'] + $attributes;
+    }
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/month
+     * @param array $attributes
+     */
+    public function typeMonth(array $attributes): void
+    {
+        $this->attributes = ['type' => 'month'] + $attributes;
+    }
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/number
+     * @param array $attributes
+     */
+    public function typeNumber(array $attributes): void
+    {
+        $this->attributes = ['type' => 'number'] + $attributes;
+    }
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/password
+     * @param array $attributes
+     */
+    public function typePassword(array $attributes): void
+    {
+        $this->attributes = ['type' => 'password'] + $attributes;
     }
 
     /**
@@ -36,12 +126,48 @@ class InputBuilder
     }
 
     /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range
+     * @param array $attributes
+     */
+    public function typeRange(array $attributes): void
+    {
+        $this->attributes = ['type' => 'range'] + $attributes;
+    }
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/reset
+     * @param array $attributes
+     */
+    public function typeReset(array $attributes): void
+    {
+        $this->attributes = ['type' => 'reset'] + $attributes;
+    }
+
+    /**
      * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/search
      * @param array $attributes
      */
     public function typeSearch(array $attributes): void
     {
         $this->attributes = ['type' => 'search'] + $attributes;
+    }
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/submit
+     * @param array $attributes
+     */
+    public function typeSubmit(array $attributes): void
+    {
+        $this->attributes = ['type' => 'submit'] + $attributes;
+    }
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/tel
+     * @param array $attributes
+     */
+    public function typeTel(array $attributes): void
+    {
+        $this->attributes = ['type' => 'tel'] + $attributes;
     }
 
     /**
@@ -54,39 +180,39 @@ class InputBuilder
     }
 
     /**
-     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/datetime-local
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/time
      * @param array $attributes
      */
-    public function typeDateTimeLocal(array $attributes)
+    public function typeTime(array $attributes): void
     {
-        $this->attributes = ['type' => 'datetime-local'] + $attributes;
+        $this->attributes = ['type' => 'time'] + $attributes;
     }
 
     /**
-     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/hidden
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/url
      * @param array $attributes
      */
-    public function typeHidden(array $attributes)
+    public function typeUrl(array $attributes): void
     {
-        $this->attributes = ['type' => 'hidden'] + $attributes;
+        $this->attributes = ['type' => 'url'] + $attributes;
     }
 
     /**
-     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/submit
-     * @param $attributes
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/week
+     * @param array $attributes
      */
-    public function typeSubmit($attributes)
+    public function typeWeek(array $attributes): void
     {
-        $this->attributes = ['type' => 'submit'] + $attributes;
+        $this->attributes = ['type' => 'week'] + $attributes;
     }
 
     /**
-     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/password
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/datetime
      * @param array $attributes
      */
-    public function typePassword(array $attributes)
+    public function typeDatetime(array $attributes): void
     {
-        $this->attributes = ['type' => 'checkbox'] + $attributes;
+        $this->attributes = ['type' => 'datetime'] + $attributes;
     }
 
     public function __invoke(): InputElement
