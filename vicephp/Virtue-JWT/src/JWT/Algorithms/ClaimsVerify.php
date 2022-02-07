ee26ed5f87a67682d2a341c4833414a3cc769ea8
Evgenii Morozov <emorozov@goodgamestudios.com>
2022-02-07 10:56:46 +0100

    verify typ, sub and iat

--- vicephp/Virtue-JWT/src/JWT/Algorithms/ClaimsVerify.php
+++ vicephp/Virtue-JWT/src/JWT/Algorithms/ClaimsVerify.php
@@ -18,34 +18,52 @@ class ClaimsVerify implements VerifiesToken
 
     public function verify(Token $token): void
     {
-        $alg = $token->header('alg');
+        $typ = $token->header('typ');
+        if ($typ !== 'JWT') {
+            throw new VerificationFailed('Only JWT tokens are allowed');
+        }
 
         $now = time();
-        $exp = $token->payload('exp');
-        $iat = $token->payload('iat');
-        $nbf = $token->payload('nbf');
+        $exp = $token->payload('exp') ?: $now;
+        $iat = $token->payload('iat') ?: $now;
+        $nbf = $token->payload('nbf') ?: $now;
 
         $issuer = $token->payload('iss');
         $audience = $token->payload('aud');
+        $subject = $token->payload('sub');
 
         if ($now > $exp) {
-            throw new VerificationFailed('Token expired');
+            throw new VerificationFailed('Token has expired');
+        }
+
+        if (isset($this->settings['iat'])) {
+            if (isset($this->settings['iat']['before']) && $iat > $this->settings['iat']['before']) {
+                throw new VerificationFailed('Token was issued after expected time');
+            }
+            if (isset($this->settings['iat']['after']) && $iat < $this->settings['iat']['after']) {
+                throw new VerificationFailed('Token was issued before expected time');
+            }
         }
 
         if ($now < $nbf) {
-            throw new VerificationFailed('Token not yet valid');
+            throw new VerificationFailed('Token is not yet valid');
         }
 
         if (isset($this->settings['issuers']) && !in_array($issuer, $this->settings['issuers'])) {
-            throw new VerificationFailed('Issuer not allowed');
+            throw new VerificationFailed('Issuer is not allowed');
         }
 
         if (isset($this->settings['audience']) && $audience !== $this->settings['audience']) {
-            throw new VerificationFailed('Audience not allowed');
+            throw new VerificationFailed('Audience is not allowed');
         }
 
+        if (isset($this->settings['subjects']) && !in_array($subject, $this->settings['subjects'])) {
+            throw new VerificationFailed('Subject is not allowed');
+        }
+
+        $alg = $token->header('alg') ?: 'none';
         if (isset($this->settings['algorithms']) && !in_array($alg, $this->settings['algorithms'])) {
-            throw new VerificationFailed('Algorithm not allowed');
+            throw new VerificationFailed('Algorithm is not allowed');
         }
     }
 
