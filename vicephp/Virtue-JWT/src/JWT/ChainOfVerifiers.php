d2cbd0b7fd37e95fa0d39722a8da240596b3cdc4
Ievgenii Skliarenko <iskliarenko@goodgamestudios.com>
2022-06-27 10:41:16 +0200

    Add possibility to compose a chain of verifiers from a set of simple verifiers

---
+++ vicephp/Virtue-JWT/src/JWT/ChainOfVerifiers.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Virtue\JWT;
+
+use Webmozart\Assert\Assert;
+
+class ChainOfVerifiers implements VerifiesToken
+{
+    /** @var \Virtue\JWT\VerifiesToken[] */
+    private $verifiers;
+
+    public function __construct(array $verifiers)
+    {
+        Assert::allIsInstanceOf($verifiers, VerifiesToken::class);
+        $this->verifiers = $verifiers;
+    }
+
+
+    public function verify(Token $token): void
+    {
+        // once we switch to php7.4+, this code can be replaced with one line
+        array_walk($this->verifiers, function (VerifiesToken $verifier) use ($token) {
+            $verifier->verify($token);
+        });
+    }
+}
