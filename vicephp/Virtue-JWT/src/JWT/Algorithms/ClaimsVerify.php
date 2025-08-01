cff10d5e27f8b2b611199fabc551e9f197133c28
Marius Schütte <marius.schuette@stillfront.com>
2025-08-01 09:24:27 +0200

    Add explicit type hints to ClaimsVerify

--- vicephp/Virtue-JWT/src/JWT/Algorithms/ClaimsVerify.php
+++ vicephp/Virtue-JWT/src/JWT/Algorithms/ClaimsVerify.php
@@ -42,16 +42,13 @@ class ClaimsVerify implements VerifiesToken
         }
 
         $now = time();
+        /** @var int $exp */
         $exp = $token->payload('exp') ?: $now;
+        /** @var int $iat */
         $iat = $token->payload('iat') ?: $now;
+        /** @var int $nbf */
         $nbf = $token->payload('nbf') ?: $now;
-
         $leeway = $this->settings['leeway'] ?? 0;
-
-        $issuer = $token->payload('iss');
-        $audience = $token->payload('aud');
-        $audience = is_string($audience) ? [$audience] : (is_array($audience) ? $audience : []);
-        $subject = $token->payload('sub');
 
         if ($now > $exp + $leeway) {
             throw new VerificationFailed('Token has expired', VerificationFailed::INVALID_TOKEN);
@@ -70,10 +67,14 @@ class ClaimsVerify implements VerifiesToken
             throw new VerificationFailed('Token is not yet valid', VerificationFailed::INVALID_TOKEN);
         }
 
+        /** @var string $issuer */
+        $issuer = $token->payload('iss');
         if (isset($this->settings['issuers']) && !in_array($issuer, $this->settings['issuers'], true)) {
             throw new VerificationFailed("Issuer '{$issuer}' is not allowed", VerificationFailed::INVALID_ISSUER);
         }
 
+        $audience = $token->payload('aud');
+        $audience = is_string($audience) ? [$audience] : (is_array($audience) ? $audience : []);
         if (isset($this->settings['audience']) && !in_array($this->settings['audience'], $audience, true)) {
             throw new VerificationFailed(
                 sprintf("Audience '%s' is not allowed", implode(', ', $audience)),
@@ -81,10 +82,13 @@ class ClaimsVerify implements VerifiesToken
             );
         }
 
+        /** @var string $subject */
+        $subject = $token->payload('sub');
         if (isset($this->settings['subjects']) && !in_array($subject, $this->settings['subjects'], true)) {
             throw new VerificationFailed("Subject '{$subject}' is not allowed", VerificationFailed::INVALID_SUBJECT);
         }
 
+        /** @var string $alg */
         $alg = $token->headers('alg', 'none');
         if (isset($this->settings['algorithms']) && !in_array($alg, $this->settings['algorithms'], true)) {
             throw new VerificationFailed("Algorithm '{$alg}' is not allowed", VerificationFailed::INVALID_ALGORITHM);
